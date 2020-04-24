5717333deb2ac5fdb6d8a749918793b126cf16ab
dehamman <davie1699@gmail.com>
2020-04-24 12:58:57 -0500

    5 files modified

--- src/app/apps.ts
+++ src/app/apps.ts
@@ -1,17 +1,52 @@
 export const apps = [
   {
     name: 'UserData',
-    description: 'lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    description: 'UserData is lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
+    data:[
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"DNS",value:"172.16.43.11"},
+      {field:"Domain",value:"CNHSA"},
+      {field:"???",value:"IUNNO"},
+      ]
   },
   {
     name: 'ChartAudit',
-    description: 'lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    description: 'ChartAudit is for nursing admin and lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
+    data:[
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      ]
   },
   {
     name: 'CNHSAIntranet',
     description: '',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    data:[
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      {field:"SME",value:"Dave Clouse"},
+      ]
   }
 ];
