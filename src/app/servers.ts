020b032de77401cbd888f2bf1d19af665e123be2
dehamman <davie1699@gmail.com>
2020-04-27 15:38:17 -0500

    5 files modified

--- src/app/servers.ts
+++ src/app/servers.ts
@@ -2,17 +2,20 @@ export const servers = [
   {
     name: 'TAWK16WWW01',
     description: 'lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},],
+    lastPatched: '1/1/2019'
   },
   {
     name: 'TAWK12DB01',
     description: 'lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah lorem epsum blah ',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},],
+    lastPatched: '2/1/2019'
   },
   {
     name: 'TAWK12WEBDEV01',
     description: '',
-    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},]
+    data:[{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},{field:"SME",value:"Dave Clouse"},],
+    lastPatched: '3/1/2019'
   }
 ];
 
